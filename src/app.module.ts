e6a9bce756050f7be71e79ac975f60ddb84d5435
marian-sofia <sofiagutierrez0031@gmail.com>
2024-01-04 17:19:49 -0500

    Merge completo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './modules/posts/posts.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [PostsModule, UsersModule],
