ee6b3b09282ce6b958e7f4b1c3140b74d5879217
marian-sofia <sofiagutierrez0031@gmail.com>
2024-01-04 17:17:33 -0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
   imports: [PostsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
