7365d16ca41e78f38b4c75f15d0b5d1e78764bd8
marian-sofia <sofiagutierrez0031@gmail.com>
2024-01-04 17:57:48 -0500

    post entity

--- src/modules/posts/entities/post.entity.ts
+++ src/modules/posts/entities/post.entity.ts
@@ -1,1 +1,37 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  title: string
+
+  @Column()
+  description: string
+
+  @Column()
+  resource: string
+
+  @Column()
+  creationDate: Date
+
+  @Column()
+  owner: string 
+
+  @Column()
+  reactions: number
+
+  @Column()
+  comments: number
+
+  @Column()
+  state: boolean
+
+  @Column()
+  tags: string
+
+  @Column()
+  spoiler: boolean
+}
