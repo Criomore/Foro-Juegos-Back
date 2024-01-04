9084d254f1f3e4556d2f7bc60e98c7aa23b0361a
jaider-nieto <jaiderlolxd@gmail.com>
2024-01-04 17:57:16 -0500

    user entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,46 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  birthDate: Date;
+
+  @Column()
+  creationDate: string;
+
+  @Column()
+  followers: number;
+
+  @Column()
+  following: number;
+
+  @Column()
+  interests: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  background: string;
+
+  @Column()
+  notifications: number;
+
+  @Column()
+  state: boolean;
+}
