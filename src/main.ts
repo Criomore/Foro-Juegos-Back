1bed8116d919eda9ac2db31cc311140680afa210
jaider-nieto <jaiderlolxd@gmail.com>
2024-01-05 20:35:50 -0500

    endpoint create y findAll de los usuarios, primeras validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3001)
 }
-bootstrap();
+bootstrap()
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,51 +1,60 @@
-import { STATE } from 'src/constants/state.enum';
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Transform } from 'class-transformer'
+import { IsDateString } from 'class-validator'
+import { STATE } from 'src/constants/state.enum'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm'
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn('increment')
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ type: 'text', nullable: false, unique: true })
+  userName: string
+
+  @Column({ type: 'text', nullable: false, unique: true })
+  email: string
 
   @Column({ type: 'text', nullable: false })
-  userName: string;
+  password: string
 
   @Column({ type: 'text', nullable: false })
-  email: string;
+  country: string
 
-  @Column({ type: 'text', nullable: false })
-  password: string;
+  @Column()
+  @IsDateString()
+  @Transform(({ value }) => value.toISOString().split('T')[0])
+  birthDate: Date
 
-  @Column({ type: 'text', nullable: false })
-  country: string;
+  @CreateDateColumn()
+  creationDate: Date
 
-  @Column({ type: 'date', nullable: false })
-  birthDate: Date;
+  @Column({ type: 'int', default: 0 })
+  followers: number
 
-  @Column({ type: 'date', nullable: false })
-  creationDate: Date;
-
-  @Column({ type: 'int', nullable: false })
-  followers: number;
-
-  @Column({ type: 'int', nullable: false })
-  following: number;
+  @Column({ type: 'int', default: 0 })
+  following: number
 
   @Column({ type: 'text', nullable: false, array: true })
-  interests: string;
+  interests: string
 
   @Column({ type: 'text', nullable: false })
-  avatar: string;
+  avatar: string
 
   @Column({
     type: 'text',
     default:
       'https://e0.pxfuel.com/wallpapers/696/294/desktop-wallpaper-grey-color-background-solid-color-background.jpg',
   })
-  background: string;
+  background: string
 
   @Column({ type: 'int', default: 0 })
-  notifications: number;
+  notifications: number
 
   @Column({ type: 'enum', enum: STATE, default: STATE.ACTIVE })
-  state: boolean;
+  state: number
 }
