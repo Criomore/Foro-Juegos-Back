d858569fccac83eab37984e5a3d869955e40e6b4
marian-sofia <sofiagutierrez0031@gmail.com>
2024-01-12 11:35:40 -0500

    Configuracion de las rutas protegidas

--- src/modules/auth/interfaces/auth.interface.ts
+++ src/modules/auth/interfaces/auth.interface.ts
@@ -1,8 +1,22 @@
 export interface PayloadToken {
   sub: string
+  //role
 }
 
 export interface AuthBody {
-  username: string,
+  username: string
   password: string
+}
+
+export interface AuthTokenResult {
+  sub: string
+  //role
+  ait: number
+  exp: number
+}
+
+export interface IUseToken {
+  sub: string
+  //role
+  isExpired: boolean
 }
